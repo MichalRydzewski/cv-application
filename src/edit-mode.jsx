f860e437f2e9d24817155940f908c8e47ae4275f
Michal <michal.rydzewski.03@gmail.com>
2025-08-25 20:33:16 +0200

    export InputTemplate helper component

--- src/edit-mode.jsx
+++ src/edit-mode.jsx
@@ -1,7 +1,7 @@
 import Contact from "./contact"
 import Education from "./education"
 import Experience from "./experience"
-import './edit-mode.css'
+import "./edit-mode.css"
 
 export default function EditMode() {
   return (
@@ -15,5 +15,18 @@ export default function EditMode() {
         <Experience></Experience>
       </main>
     </>
+  )
+}
+
+export function InputTemplate({ labelName, inputType }) {
+  const id = labelName
+    .split(" ")
+    .map((word) => word[0].toLowerCase() + word.slice(1))
+    .join("-")
+  return (
+    <div className="input-flex">
+      <label htmlFor={id}>{labelName}</label>
+      <input type={inputType} id={id} />
+    </div>
   )
 }
