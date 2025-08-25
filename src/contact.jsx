e0e304adfb7224e9428931cdb8bfcb3e2ec1dbfa
Michal <michal.rydzewski.03@gmail.com>
2025-08-25 14:25:49 +0200

    start styling edit mode, finish contact section

--- src/contact.jsx
+++ src/contact.jsx
@@ -1,14 +1,28 @@
 export default function Contact() {
-  function template(x) {
-    return 'Enter your ' + x
+  function Template({ labelName, inputType }) {
+    const id = labelName.split(" ").map((word) => word[0].toLowerCase() + word.slice(1)).join("-")
+    return (
+      <div className="input-flex">
+        <label htmlFor={id}>{labelName}</label>
+        <input type={inputType} id={id} />
+      </div>
+    )
   }
 
   return (
-    <>
-      <h2>Contact info</h2>
-      <input type="text" placeholder={template("name")}></input>
-      <input type="text" placeholder={template("surname")}></input>
-      <input type="tel" placeholder={template("number")}></input>
-    </>
+    <div className="general">
+      <h2>General information</h2>
+      <div className="general-grid">
+        <Template labelName={"First Name"} inputType={"text"}></Template>
+        <Template labelName={"Last Name"} inputType={"text"}></Template>
+        <Template labelName={"Email"} inputType={"email"}></Template>
+        <Template labelName={"Number"} inputType={"tel"}></Template>
+      </div>
+      {/* <div className="contact-grid">
+        <Template labelName={"Your school"} inputType={"text"}></Template>
+        <Template labelName={"Title of study"} inputType={"text"}></Template>
+        <Template labelName={"Your school"} inputType={"text"}></Template>
+      </div> */}
+    </div>
   )
 }
--- src/edit-mode.jsx
+++ src/edit-mode.jsx
@@ -1,13 +1,19 @@
 import Contact from "./contact"
 import Education from "./education"
 import Experience from "./experience"
+import './edit-mode.css'
 
 export default function EditMode() {
   return (
     <>
-      <Contact></Contact>
-      <Education></Education>
-      <Experience></Experience>
+      <header>
+        <h1>CV Creator</h1>
+      </header>
+      <main>
+        <Contact></Contact>
+        <Education></Education>
+        <Experience></Experience>
+      </main>
     </>
   )
 }
