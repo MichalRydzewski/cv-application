75f7bd3ce7fb117c845ab12066d9842c77e287a7
Michal <michal.rydzewski.03@gmail.com>
2025-08-26 21:24:42 +0200

    add grid textarea which is now responsive

--- src/experience.jsx
+++ src/experience.jsx
@@ -9,17 +9,33 @@ export default function Experience() {
           labelName={"Company name"}
           inputType={"text"}
         ></InputTemplate>
+        <div className="input-flex">
+          <label htmlFor="main-responsibilities">Main responsibilities</label>
+          <textarea
+            id="main-responsibilities"
+            placeholder="Please enter each one in a new line like so:
+
+doing some computer stuff
+assembling lego
+eating pizza 
+fishing in troubled waters"
+          />
+        </div>
         <InputTemplate
           labelName={"Position title"}
           inputType={"text"}
         ></InputTemplate>
-        <InputTemplate
-          labelName={"Main responsibilities"}
-          inputType={"text"}
-        ></InputTemplate>
         <div className="date-box">
-          <InputTemplate id="experience-from" labelName={"From"} inputType={"date"}></InputTemplate>
-          <InputTemplate id="experience-to" labelName={"To"} inputType={"date"}></InputTemplate>
+          <InputTemplate
+            id="experience-from"
+            labelName={"From"}
+            inputType={"date"}
+          ></InputTemplate>
+          <InputTemplate
+            id="experience-to"
+            labelName={"To"}
+            inputType={"date"}
+          ></InputTemplate>
         </div>
       </div>
     </section>
