7c36619b3ea6e3ad66df4e4a40650c0e20be6b4a
Michal <michal.rydzewski.03@gmail.com>
2025-08-26 22:08:53 +0200

    add switching between edit and display modes

--- src/app.jsx
+++ src/app.jsx
@@ -1,13 +1,24 @@
 import EditMode from "./edit-mode"
 import ViewMode from "./view-mode"
+import { useState } from "react"
 
 export default function App() {
+  const [mode, setMode] = useState("edit")
 
-  return (
+  function handleMode(e) {
+    e.preventDefault()
+    setMode(e.target.value)
+  }
+
+  return mode === "edit" ? (
     <>
       <EditMode></EditMode>
+      <button value="view" onClick={handleMode}>Enter View Mode</button>
+    </>
+  ) : (
+    <>
       <ViewMode></ViewMode>
-      <button>Enter View Mode</button>
+      <button value="edit" onClick={handleMode}>Enter Edit Mode</button>
     </>
   )
 }
